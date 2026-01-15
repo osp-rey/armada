ceb87c9417db4a697b072c3bdf90c13868ea7822
pauchok-sk <pauchoooooook@gmail.com>
2026-01-15 13:40:34 +0300

    burger

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,9 @@
 import "../scss/style.scss";
+import burger from "./files/burger.js";
 import spoller from "./files/spoller.js";
 
-spoller();
+burger();
+Fancybox.bind("[data-fancybox]", {
+  closeButton: false,
+});
+// Fancybox.show([{type: "inline", src: "#modal-order"}])
---
+++ src/js/files/burger.js
@@ -0,0 +1,46 @@
+export default function burger() {
+  const burgerOpen = document.querySelector("#burger-open");
+  const burgerClose = document.querySelector("#burger-close");
+  const burger = document.querySelector("#burger");
+  const burgerOverlay = document.querySelector("#burger-overlay");
+
+  if (burger) {
+    const menuButtons = burger.querySelectorAll(".burger__list a");
+
+    menuButtons.forEach((btn) =>
+      btn.addEventListener("click", handlerBurgerClose)
+    );
+
+    burger.addEventListener("click", (e) => e.stopPropagation());
+
+    burgerOverlay.addEventListener("click", handlerBurgerClose);
+
+    burgerOpen.addEventListener("click", (e) => {
+      e.stopPropagation();
+
+      if (burgerOpen.classList.contains("_active")) {
+        handlerBurgerClose();
+      } else {
+        handlerBurgerOpen();
+      }
+    });
+    burgerClose.addEventListener("click", (e) => {
+      e.stopPropagation();
+      handlerBurgerClose();
+    });
+
+    function handlerBurgerClose() {
+      burgerOpen.classList.remove("_active");
+      burger.classList.remove("_open");
+      burgerOverlay.classList.remove("_active");
+      document.body.classList.remove("body-hidden");
+    }
+
+    function handlerBurgerOpen() {
+      burgerOpen.classList.add("_active");
+      burger.classList.add("_open");
+      burgerOverlay.classList.add("_active");
+      document.body.classList.add("body-hidden");
+    }
+  }
+}
